6f065d44aa7c0a250eea25f3698023019d0e5afb
JanKuchyna <jankuchyna@protonmail.com>
2026-04-17 18:33:32 +0200

    updated

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  UMAMI_SCRIPT_URL: 'https://cloud.umami.is/script.js',
+  UMAMI_SCRIPT_URL: 'https://umami.firmhound.cz/script.js',
   UMAMI_WEBSITE_ID: 'REPLACE_WITH_UMAMI_WEBSITE_ID',
   API_URL: 'http://localhost:5000',
 };
